ab1edbef7747ddfcc0d127d9fb6b6efef249078e
Arthur Santana <arthurgiannonisantana@yahoo.com.br>
2023-06-29 21:25:14 -0300

    [FEATURE] - Inserindo sidebar na tela de apostas

--- src/app/modules/components/contest/contest.module.ts
+++ src/app/modules/components/contest/contest.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from "@angular/common";
 import { ContestComponent } from "./contest.component";
 import { ContestRoutingModule } from "./contest-routing.module";
 import { SubHeaderComponent } from "src/app/shared/components/sub-header/sub-header.component";
+import { SidebarComponent } from "src/app/shared/components/sidebar/sidebar.component";
 
 @NgModule({
   declarations: [ContestComponent],
-  imports: [CommonModule, ContestRoutingModule, SubHeaderComponent]
+  imports: [
+    CommonModule,
+    ContestRoutingModule,
+    SubHeaderComponent,
+    SidebarComponent
+  ]
 })
 export class ContestModule {}
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: "loto-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"]
+})
+export class SidebarComponent {}
