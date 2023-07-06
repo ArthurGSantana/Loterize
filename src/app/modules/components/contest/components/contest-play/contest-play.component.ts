243bbec1e1c1821392e6e286e422a2cc8d2a0578
Arthur Santana <arthurgiannonisantana@yahoo.com.br>
2023-07-05 21:00:50 -0300

    [FEATURE] - Criando componente principal das apostas

---
+++ src/app/modules/components/contest/components/contest-play/contest-play.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loto-contest-play',
+  templateUrl: './contest-play.component.html',
+  styleUrls: ['./contest-play.component.scss']
+})
+export class ContestPlayComponent {
+
+}
--- src/app/modules/components/contest/contest.module.ts
+++ src/app/modules/components/contest/contest.module.ts
@@ -5,9 +5,10 @@ import { ContestComponent } from "./contest.component";
 import { ContestRoutingModule } from "./contest-routing.module";
 import { SubHeaderComponent } from "src/app/shared/components/sub-header/sub-header.component";
 import { SidebarComponent } from "src/app/shared/components/sidebar/sidebar.component";
+import { ContestPlayComponent } from './components/contest-play/contest-play.component';
 
 @NgModule({
-  declarations: [ContestComponent],
+  declarations: [ContestComponent, ContestPlayComponent],
   imports: [
     CommonModule,
     ContestRoutingModule,
