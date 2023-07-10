50e24f7783c395c82f4c51ffbb3c0627a7d7f642
Arthur Santana <arthurgiannonisantana@yahoo.com.br>
2023-07-10 20:40:32 -0300

    [FEATURE] - Implementando Signals para compor logica de selecao

--- src/app/shared/components/contest-numbers/contest-numbers.component.ts
+++ src/app/shared/components/contest-numbers/contest-numbers.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
+import {
+  Component,
+  Input,
+  Signal,
+  WritableSignal,
+  computed,
+  signal
+} from "@angular/core";
 
 @Component({
   standalone: true,
@@ -16,4 +23,28 @@ export class ContestNumbersComponent {
     const number = index + 1;
     return number < 10 ? `0${number}` : `${number}`;
   });
+
+  selectedNumbers: WritableSignal<string[]> = signal([]);
+  sortSelectedNumbers: Signal<string[]> = computed(() =>
+    this.selectedNumbers().sort((a, b) => {
+      return parseInt(a, 10) - parseInt(b, 10);
+    })
+  );
+
+  handleSelectedNumbers(item: string): void {
+    let size: number = this.selectedNumbers().length;
+
+    if (this.selectedNumbers().includes(item)) {
+      this.selectedNumbers.update((value) => {
+        let index = value.indexOf(item);
+
+        value.splice(index, 1);
+        return [...value];
+      });
+    } else {
+      if (size < this.maxNumbersSelected) {
+        this.selectedNumbers.update((value) => [...value, item]);
+      }
+    }
+  }
 }
